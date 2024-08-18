f8e965b9f0e6364339fe112e431f41ecb8b1e630
Outer Cloud Studio <outercloudstudio@gmail.com>
2024-08-18 13:32:10 -0500

    tool buttons

---
+++ scenes/tool_button.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+
+@export var texture: Texture2D
+@export var is_enabled = false
+var _is_enabled_cache = false
+
+
+signal on_clicked()
+
+
+func _ready() -> void:
+	$TextureRect.texture = texture
+
+
+func _process(_delta):
+	if _is_enabled_cache != is_enabled:
+		if is_enabled:
+			$AnimationPlayer.play("show")
+		else:
+			$AnimationPlayer.play("hide")
+
+		_is_enabled_cache = is_enabled
+
+
+func clicked() -> void:
+	if !is_enabled:
+		return
+
+	on_clicked.emit()
+	
